b64ccf67913023fa19c22aeb40e9e02f301f60ee
RTamilKing <tamilking337010@gmail.com>
2022-03-26 21:39:56 +0530

    New Java Project

---
+++ src/org/test/Sample.java
@@ -0,0 +1,20 @@
+package org.test;
+
+public class Sample {
+
+	private void add() {
+
+		System.out.println("add ");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		Sample s = new Sample();
+		
+		s.add();
+		
+	}
+
+}
