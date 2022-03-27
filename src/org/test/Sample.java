8d48e1acda2f09409a36f5e19090af245f38d36f
RTamilKing <tamilking337010@gmail.com>
2022-03-27 18:53:02 +0530

    New task is done

--- src/org/test/Sample.java
+++ src/org/test/Sample.java
@@ -2,19 +2,29 @@ package org.test;
 
 public class Sample {
 
-	private void add() {
+	public void add() {
 
 		System.out.println("add ");
 
 	}
-	
+
+	public void at() {
+		System.out.println("att");
+	}
+
+	public void numaric() {
+		System.out.println("numaric");
+	}
+
 	public static void main(String[] args) {
-		
-		
+
 		Sample s = new Sample();
-		
+
 		s.add();
+		s.numaric();
+		s.at();
 		
+
 	}
 
 }
